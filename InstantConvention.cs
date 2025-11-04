d71834312b81292db901fbfcce55635bc4220ff3
Homayoun <eqpc@hbn.dev>
2025-11-04 21:38:12 +0100

    Add project files.

---
+++ InstantConvention.cs
@@ -0,0 +1,21 @@
+﻿using FluentNHibernate.Conventions;
+using FluentNHibernate.Conventions.AcceptanceCriteria;
+using FluentNHibernate.Conventions.Inspections;
+using FluentNHibernate.Conventions.Instances;
+using NodaTime;
+
+namespace NHibernate.NodaTime;
+
+public class InstantConvention : IPropertyConvention, IPropertyConventionAcceptance
+{
+    public void Accept(IAcceptanceCriteria<IPropertyInspector> criteria)
+    {
+        criteria.Expect(x => x.Property.PropertyType == typeof(Instant) ||
+                            x.Property.PropertyType == typeof(Instant?));
+    }
+
+    public void Apply(IPropertyInstance instance)
+    {
+        instance.CustomType<InstantType>();
+    }
+}
