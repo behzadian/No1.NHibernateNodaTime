6c6c74d0feb16daf0b9fc88e6b6a3456a28e03c0
Hank <eqpc@hbn.dev>
2026-05-11 10:06:42 +0200

    testing tests execution on GH

--- lib/YearMonthCompositeUserType.cs
+++ lib/YearMonthCompositeUserType.cs
@@ -34,7 +34,7 @@ public sealed class YearMonthCompositeUserType : ICompositeUserType
 	object? ICompositeUserType.NullSafeGet(DbDataReader dr, string[] names, ISessionImplementor session, object owner)
 	{
 
-		var counter = 0;
+		var counter = 1;
 
 		if (dr[names[counter++]] is not string eraId)
 			return null;
